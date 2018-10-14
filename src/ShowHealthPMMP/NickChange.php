0c9b68e1dcfff684bf71fe4140d4fc78191001fe
ProxyTechDevs <44120635+ProxyTechDevs@users.noreply.github.com>
2018-10-14 01:56:45 -0400

    Update NickChange.php

--- src/ShowHealthPMMP/NickChange.php
+++ src/ShowHealthPMMP/NickChange.php
@@ -1,1 +1,22 @@
+<?php
+namespace ShowHealthPMMP;
+use pocketmine\event\Listener;
+use EssentialsPE\Events\PlayerNickChangeEvent;
+use ShowHealthPMMP\Main;
 
+class NickChange implements Listener {
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+    }
+    
+    public function onNickChange(PlayerNickChangeEvent $event) {
+        $this->plugin->broadcastMessage("A Players Nick Has Changed");
+        $this->player = $event->getPlayer();
+        $this->config = $this->plugin->getConfig()->getAll();
+        if($this->config["Nametag"]["Enabled"] === true) {
+            $this->plugin->getScheduler()->scheduleDelayedTask(new Task($this, $this->player), 1);
+        }
+    }
+
+}
