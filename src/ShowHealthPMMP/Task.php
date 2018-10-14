0e4dbb37c278cbafd4306e377ddcc0b272223df7
ProxyTechDevs <44120635+ProxyTechDevs@users.noreply.github.com>
2018-10-14 01:57:34 -0400

    Update Task.php

--- src/ShowHealthPMMP/Task.php
+++ src/ShowHealthPMMP/Task.php
@@ -1,1 +1,22 @@
+<?php
+namespace ShowHealthPMMP;
+use pocketmine\scheduler\Task as PluginTask;
+use pocketmine\Player;
+use ShowHealthPMMP\Main;
+use pocketmine\entity\Entity;
+use pocketmine\utils\TextFormat;
 
+class Task extends PluginTask{
+
+    public function __construct(Main $plugin, Player $player){
+        $this->plugin = $plugin;
+        $this->player = $player;
+    }
+   
+	public function onRun(int $tick){
+		foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
+      $prcnt = ($player->getHealth()/ $player->getMaxHealth()) * 20;
+      $player->getDataPropertyManager()->setString(Entity::DATA_SCORE_TAG, TextFormat::BOLD . TextFormat::RED."❤ ".$prcnt."%");
+		}
+	}
+}
