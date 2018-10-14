df82c812b40e5e270028f2b7055920f7c26b8332
ProxyTechDevs <44120635+ProxyTechDevs@users.noreply.github.com>
2018-10-14 01:53:21 -0400

    Update GroupChange.php

--- src/ShowHealthPMMP/GroupChange.php
+++ src/ShowHealthPMMP/GroupChange.php
@@ -1,1 +1,21 @@
+<?php
+namespace ShowHealthPMMP;
+use pocketmine\event\Listener;
+use _64FF00\PurePerms\event\PPGroupChangedEvent;
+use ShowHealthPMMP\Main;
 
+class GroupChange implements Listener {
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+    }
+    
+    public function onGroupChange(PPGroupChangedEvent $event) {
+        $this->player = $event->getPlayer();
+        $this->config = $this->plugin->getConfig()->getAll();
+        if($this->config["Nametag"]["Enabled"] === true) {
+            $this->plugin->getScheduler()->scheduleDelayedTask(new Task($this->plugin, $this->player), 1);
+        }
+    }
+
+}
